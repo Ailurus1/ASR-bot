99b43e1414d6356653ab2871c466a831827d3243
Ailurus1 <kozulin02@yandex.ru>
2024-12-15 20:57:21 +0300

    wip

--- inference_server/__main__.py
+++ inference_server/__main__.py
@@ -1,91 +1,103 @@
 from fastapi import FastAPI, File, UploadFile
 from fastapi.responses import JSONResponse
 import uvicorn
-from io import BytesIO
 import torch
-import logging
-import torchaudio
-from transformers import WhisperProcessor, WhisperForConditionalGeneration
+from model import ASRModel
+from profiles import PROFILES
 import asyncio
+from asyncio import Future
+from asyncio.queues import Queue
+from typing import Callable, Generic, TypeVar, Tuple, List, Optional
 
 app = FastAPI()
 
-device = "cuda" if torch.cuda.is_available() else "cpu"
+T = TypeVar("T")
+U = TypeVar("U")
 
-model_name = "openai/whisper-tiny"
-processor = WhisperProcessor.from_pretrained(model_name)
-model = WhisperForConditionalGeneration.from_pretrained(model_name).to(device)
-model.config.forced_decoder_ids = processor.get_decoder_prompt_ids(
-    language="russian", task="transcribe"
+class DataCollator:
+    def __init__(self, stack: bool = False) -> None:
+        super().__init__()
+        self.stack = stack
+
+    def collate(self, inputs: List[T]) -> T:
+        if self.stack is not None:
+            return torch.stack(inputs)
+
+        return torch.cat(inputs)
+
+    def uncollate(self, input: T) -> List[T]:
+        return [x if self.stack else x.unsqueeze(0) for x in input]
+
+
+class BatchedServer(Generic[T, U]):
+    def __init__(
+        self,
+        inference_callable: Callable[[T], U],
+        batch_size: int,
+        collator: Optional[DataCollator[T]] = None,
+    ) -> None:
+        self.queue: Queue[Tuple[T, Future[U], float]] = Queue(
+            maxsize=2 * batch_size
+        )
+        self.inference_callable = inference_callable
+        self.batch_size = batch_size
+        self.collator = collator if collator is not None else DataCollator()
+
+    async def submit(self, input: T) -> U:
+        loop = asyncio.get_running_loop()
+        future = loop.create_future()
+        await self.queue.put((input, future, loop.time()))
+        return await future
+
+    async def queue_processing(self):
+        loop = asyncio.get_running_loop()
+
+        while True:
+            if not self.queue.empty():
+                batch_size = min(self.queue.qsize(), self.batch_size)
+                inputs_list: List[T] = []
+                futures: List[Future[U]] = []
+                for _ in range(batch_size):
+                    input, future, _ = self.queue.get_nowait()
+                    inputs_list.append(input)
+                    futures.append(future)
+                inputs = self.collator.collate(inputs_list)
+
+                try:
+                    outputs = await asyncio.to_thread(self.inference_callable, inputs)
+                    outputs_list = self.collator.uncollate(outputs)
+                    for output, future in zip(outputs_list, futures):
+                        future.set_result(output)
+                except Exception as exc:
+                    for future in futures:
+                        future.set_exception(Exception("Could not process batch"))
+            else:
+                await asyncio.sleep(0.01)
+
+    def run(self):
+        loop = asyncio.get_running_loop()
+        loop.create_task(self.queue_processing())
+
+def inference(audio_bytes):
+    return app.state.asr_model.transcribe(audio_bytes)
+
+batched_server = BatchedServer(
+    inference,
+    batch_size=8,
 )
-
-batch_size = 4
-
-logger = logging.getLogger()
-log_config = uvicorn.config.LOGGING_CONFIG
-log_config["formatters"]["access"]["fmt"] = "%(asctime)s - %(levelname)s - %(message)s"
-
-audio_queue = asyncio.Queue()
-transcriptions_dict = {}
-
-is_processing = False
-
-async def process_audio_batch():
-    global is_processing
-    while True:
-        audio_files_batch = []
-        ids_batch = []
-
-        for _ in range(batch_size):
-            try:
-                audio_bytes, request_id = await asyncio.wait_for(audio_queue.get(), timeout=10)
-                audio_files_batch.append(audio_bytes)
-                ids_batch.append(request_id)
-            except asyncio.TimeoutError:
-                break
-
-        if audio_files_batch:
-            is_processing = True
-            await transcribe_audio_batch(audio_files_batch, ids_batch)
-            is_processing = False
-
-async def transcribe_audio_batch(audio_files, request_ids):
-    audio_data_list = []
-
-    for audio_bytes in audio_files:
-        audio_data, sample_rate = torchaudio.load(BytesIO(audio_bytes))
-        if sample_rate != 16000:
-            resampler = torchaudio.transforms.Resample(orig_freq=sample_rate, new_freq=16000)
-            audio_data = resampler(audio_data)
-
-        audio_data_list.append(audio_data.squeeze().numpy())
-
-    input_features = processor(
-        audio_data_list, return_tensors="pt", padding=True, sampling_rate=16000
-    ).input_features.to(device)
-
-    with torch.no_grad():
-        predicted_ids = model.generate(input_features)
-
-    transcriptions = processor.batch_decode(predicted_ids, skip_special_tokens=True)
-
-    for request_id, transcription in zip(request_ids, transcriptions):
-        transcriptions_dict[request_id] = transcription
 
 @app.post("/asr/")
 async def transcribe_audio(audio_message: UploadFile = File(...)):
-    audio_bytes = await audio_message.read()
-    request_id = id(audio_bytes)
-    await audio_queue.put((audio_bytes, request_id))
+    try:
+        audio_bytes = await audio_message.read()
+        transcription = batched_server.submit(audio_bytes)
+        return JSONResponse(content={"transcription": transcription})
 
-    while is_processing:
-        continue
+    except Exception as e:
+        return JSONResponse(content={"error": str(e)}, status_code=500)
 
-    transcription_text = transcriptions_dict.get(request_id, "Transcription is not available yet.")
-
-    return JSONResponse(content={"transcription": transcription_text})
 
 if __name__ == "__main__":
-    loop = asyncio.get_event_loop()
-    loop.create_task(process_audio_batch())
-    uvicorn.run(app, host="0.0.0.0", port=9090, log_config=log_config)
+    config = PROFILES["classical-tiny"]
+    app.state.asr_model = ASRModel(config)
+    uvicorn.run(app, host="0.0.0.0", port=9090, log_level="debug")
